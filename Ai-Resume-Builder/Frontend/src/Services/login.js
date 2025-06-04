ac29afde08046450a8769f9dec01897f7484af40
Priyanshu <priyanshudeoli65@gmail.com>
2025-06-04 09:00:45 +0530

    Added Ai-Resume-Builder as a normal folder (removed sub-repo)

---
+++ Ai-Resume-Builder/Frontend/src/Services/login.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+// import { VITE_APP_URL } from "@/config/config";
+console.log("VITE_APP_URL:", import.meta.env.VITE_APP_URL);
+console.log("VITE_APP_URL:", import.meta.env.VITE_APP_URL);  // Check the output in the console
+
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_APP_URL + "api/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+const startUser = async () => {
+  try {
+    const response = await axiosInstance.get("users/");
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error?.response?.data?.message || error?.message || "Something Went Wrong"
+    );
+  }
+};
+
+const registerUser = async (data) => {
+  try {
+    const response = await axiosInstance.post("users/register/", data);
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error?.response?.data?.message || error?.message || "Something Went Wrong"
+    );
+  }
+};
+
+const loginUser = async (data) => {
+  try {
+    const response = await axiosInstance.post("users/login/", data);
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error?.response?.data?.message || error?.message || "Something Went Wrong"
+    );
+  }
+};
+
+const logoutUser = async () => {
+  try {
+    const response = await axiosInstance.get("users/logout/");
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error?.response?.data?.message || error?.message || "Something Went Wrong"
+    );
+  }
+};
+
+export { startUser, registerUser, loginUser, logoutUser };
